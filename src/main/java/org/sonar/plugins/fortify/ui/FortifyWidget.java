f478f4f8350216f582ce587e38eda743cc0cee4c
Simon Brandhof <simon.brandhof@gmail.com>
2013-03-06 18:08:50 +0100

    SONARPLUGINS-2655 Add setting to disable report link

--- src/main/java/org/sonar/plugins/fortify/ui/FortifyWidget.java
+++ src/main/java/org/sonar/plugins/fortify/ui/FortifyWidget.java
@@ -21,7 +21,13 @@ package org.sonar.plugins.fortify.ui;
 
 import org.sonar.api.web.AbstractRubyTemplate;
 import org.sonar.api.web.RubyRailsWidget;
+import org.sonar.api.web.WidgetProperties;
+import org.sonar.api.web.WidgetProperty;
+import org.sonar.api.web.WidgetPropertyType;
 
+@WidgetProperties({
+    @WidgetProperty(key="enableReportLink", type= WidgetPropertyType.BOOLEAN, defaultValue = "true")
+})
 public class FortifyWidget extends AbstractRubyTemplate implements RubyRailsWidget {
 
   public String getId() {
