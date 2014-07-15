c85498d46c2fd4719d3e8f8db62087185e3df898
Simon Brandhof <simon.brandhof@sonarsource.com>
2014-07-15 13:42:57 +0200

    Add FortifyWidgetTest

---
+++ sonar-fortify-plugin/src/test/java/org/sonar/fortify/ui/FortifyWidgetTest.java
@@ -0,0 +1,42 @@
+/*
+ * Fortify Plugin for SonarQube
+ * Copyright (C) 2014 Vivien HENRIET and SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.fortify.ui;
+
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class FortifyWidgetTest {
+
+  @Test
+  public void test_rails_template() throws Exception {
+    FortifyWidget widget = new FortifyWidget();
+    assertThat(widget.getClass().getResource(widget.getTemplatePath()))
+      .as("Template not found: " + widget.getTemplatePath())
+      .isNotNull();
+  }
+
+  @Test
+  public void test_metadata() throws Exception {
+    FortifyWidget widget = new FortifyWidget();
+    assertThat(widget.getId()).containsIgnoringCase("fortify");
+    assertThat(widget.getTitle()).containsIgnoringCase("fortify");
+  }
+}
